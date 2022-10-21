7888b1f8c9171a00dfccd1708a71e692730742f4
Yago <yagoponcem@gmail.com>
2022-10-21 15:11:32 +0200

    profile-edit routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -13,9 +13,31 @@ router.get("/", async (req, res, next) => {
         next(err)
     }
     
-
 })
 
+//GET "/profile/edit/:userId" => USER: render a form to edit user profile
+router.get("/edit/:userId", async (req, res, next) => {
+        const { userId } = req.params
+    try {
+        const userDetails = await User.findById(userId)
+        res.render("profile/profile-edit.hbs", {userDetails})
+    } catch(err) {
+        next(err)
+    }
+})
+
+//POST "profile/edit/:userId" => recieve data to update profile's user.
+router.get("/edit/:userId", async (req, res, next) => {
+    const { userId } = req.params
+    const { firstName, lastName, photo, email, role, department, interest } = req.body
+    const newUserDetails = { firstName, lastName, photo, email, role, department, interest } 
+    try {
+        await User.findByIdAndUpdate(userId, { newUserDetails } )
+        res.redirect("/profile")
+    } catch (error) {
+        next(err)
+    }
+})
 
 
 module.exports = router;
