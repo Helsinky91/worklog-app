074419378e2652bb692b9bc096bf857d9a618cf5
Helena Saborit <helena.sb91@gmail.com>
2022-10-22 12:52:34 +0200

    event routes and views finish

--- models/Event.model.js
+++ models/Event.model.js
@@ -2,10 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const eventSchema = new Schema(
   {
-<<<<<<< HEAD
-
-=======
->>>>>>> c3afecb58bbf20d4a1ad4183f256c6393095ae90
     name: String,
     eventType: { 
       type: String,
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res, next) => {
         const userDetails = await User.findById(req.session.activeUser._id)     
 
         const worklogDates = await Log.find({"user": userDetails}).limit(1).populate("user")
-        console.log("this is worlogdates:", worklogDates)
+       // console.log("this is worlogdates:", worklogDates)
         
         res.render("profile/profile.hbs", {userDetails, worklogDates})
 
@@ -25,11 +25,7 @@ router.post("/", async (req, res, next) => {
     //funcionalidad botón worklog para que fiche con la hora con el Log.model
         
     const {timeIn, timeOut, comment, validation, user, isWorking} = req.body;
-<<<<<<< HEAD
     
-=======
-        
->>>>>>> c3afecb58bbf20d4a1ad4183f256c6393095ae90
     try {
         //pendiente: ruta para cambiar isWorking de false a true
         
@@ -47,8 +43,6 @@ router.post("/", async (req, res, next) => {
     } catch (error) {
     next(error)
     }
-
-
 })
 
 
